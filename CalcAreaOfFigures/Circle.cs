0149babae8a489d18b5a747c7e4b49575e65345f
HOME-PC\Sulimov <sulimov90@gmail.com>
2024-02-23 23:38:37 +0400

    Inition

---
+++ CalcAreaOfFigures/Circle.cs
@@ -0,0 +1,10 @@
+﻿namespace CalcAreaOfFigures
+{
+    public class Circle
+    {
+        public double CalcArea(double radius) 
+        {
+            return (Math.PI)*Math.Pow(radius, 2);
+        }
+    }
+}
---
+++ CalcAreaOfFigures/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcAreaOfFigures
+{
+    public class Triangle
+    {
+        public double CalcArea(double side1, double side2, double side3)
+        {
+            double p = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(p*(p-side1)*(p-side2)*(p-side3));
+        }
+    }
+}
